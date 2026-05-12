6315dc1f9082c8f1322c032bfeb92b389140b6b8
fajar <nugrahafajar37@gmail.com>
2026-05-12 12:34:50 +0700

    chore: update package.json and tsconfig.json for sideEffects and compiler options; refactor index.ts and tsup.config.ts for improved module exports and configuration

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,32 +1,18 @@
-import { glob } from "glob";
 import { defineConfig } from "tsup";
 
-export default defineConfig([
-  {
-    entryPoints: glob
-      .sync("./src/**/*.{ts,js,esm,cjs,tsx,jsx,json,yaml,yml,html,css}")
-      .filter((path) => !/(\.test\.ts|\.spec\.ts)$/.test(path))
-      .map((path) => path.replaceAll("\\", "/")),
-    tsconfig: "./tsconfig.json",
-    outDir: "dist/",
-    format: ["cjs", "esm"],
-    dts: true,
-    minify: true,
-    clean: true,
-    sourcemap: false,
-    bundle: true,
-    splitting: false,
-    outExtension({ format }) {
-      return {
-        dts: ".d.ts",
-        js: format === "cjs" ? ".cjs" : ".mjs",
-      };
-    },
-    treeshake: false,
-    target: "es2020",
-    platform: "neutral",
-    cjsInterop: true,
-    keepNames: true,
-    skipNodeModulesBundle: false,
-  },
-]);
+export default defineConfig({
+  entry: ["src/index.ts"],
+  format: ["cjs", "esm"],
+  dts: true, // Akan menghasilkan dist/index.d.ts yang sudah ter-merge
+  minify: true,
+  clean: true,
+  splitting: true,
+  treeshake: true,
+  target: "es2020",
+  platform: "neutral", // Agar aman digunakan di Browser, Node, Bun, dan Deno
+  outExtension({ format }) {
+    return {
+      js: format === "cjs" ? ".cjs" : ".mjs",
+    };
+  }
+});
